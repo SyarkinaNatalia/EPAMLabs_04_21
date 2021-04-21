0ab3edfefea9128e8f74208816c585ee8057f8bb
Natalia <rihter.rzn@gmail.com>
2021-04-21 22:37:35 +0300

    End HW

---
+++ Lab1/Lab1_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double total;
+            char oper;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите оператор");
+            oper = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine("Сумма чисел равна " + total);
+            }
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Разность чисел равна " + total);
+
+            }
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Произведение чисел равно " + total);
+
+            }
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine("Частное чисел равно " + total);
+
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 7, 9 };
+
+            int a;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        a = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = a;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
